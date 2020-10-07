0e68d23c98edbb224690e0d79bd0ed59d0cc3d60
Tom <tom.robson@hotmail.com>
2020-10-07 15:13:56 -0300

    Ambiente configurado

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default () => (
+    <View style={style.App}>
+        
+    </View>
+)
+
+const style = StyleSheet.create({
+    App: {
+        backgroundColor: '#AAA',
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    }
+})
